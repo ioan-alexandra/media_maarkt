9e663f3822776c8930cf882f3692b35a8269e2c4
Ioan,Alexandra A.D <alexandra.ioan@student.fontys.nl>
2021-04-14 21:38:34 +0300

    a

--- views/profilePage.php
+++ views/profilePage.php
@@ -14,7 +14,7 @@
       <br />
       <button><i class="fa fa-user"></i> Profile page</button> <br />
       <button><i class="fa fa-calendar"></i> Schedule</button>
-      <button><span class="iconify" data-icon="fa-solid:door-open" data-inline="false"></span> Log out</button>
+      <button type="button"><span class="iconify" data-icon="fa-solid:door-open" data-inline="false"></span> Log out</button>
     </div>
 
     <form class="content">
