3e46e41cda8e56821cd7a351b7cb26db5b7c1a73
Ioan,Alexandra A.D <alexandra.ioan@student.fontys.nl>
2021-04-14 16:34:42 +0300

    a

--- views/forgotPassword.php
+++ views/forgotPassword.php
@@ -12,7 +12,7 @@
 
           <h1>Please insert your email address</h1>
 
-          <form class="gform" method = "POST" action = "https://script.google.com/macros/s/AKfycbzzjArFfBS6BYW_0AVFj6PqbIo2EaI2We7i0NapAY5aWfyP3_T1MLIn9agyxzkj_hRq/exec">
+          <form class="gform" method = "POST" action = "https://script.google.com/macros/s/AKfycbz-Ci3MZTczfwD0vzoWaJghhTR5FTTHWtnA2hSkA_a1tNJh88c/exec">
              <input type="email" name = "email" placeholder="Email Address"> <br>
              <button type = "submit">Send</button>
              <a href = "views/forgotPassword.php" >Forgot password?</a>
